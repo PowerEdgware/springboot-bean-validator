f65c539eb14f9b518c05d45476a8090d798a12f8
allger <407381392@qq.com>
2020-04-22 21:57:19 +0800

    添加embbed-tomcat分析

---
+++ src/main/java/com/study/tomcat/TomcatStarter.java
@@ -0,0 +1,32 @@
+package com.study.tomcat;
+
+import org.apache.catalina.startup.Bootstrap;
+import org.apache.coyote.http11.Http11Protocol;
+import org.springframework.boot.web.servlet.ServletContextInitializerBeans;
+
+public class TomcatStarter {
+
+	public static void main(String[] args) {
+//		Bootstrap
+		//Tomcat
+		//Catalina
+		//StandardServer
+		//StandardService
+		
+//		Http11Protocol
+		//Http11NioProtocol
+		
+		
+		
+		//springboot tomcat
+		//TomcatServletWebServerFactory
+		//SpringServletContainerInitializer
+		//TomcatStarter
+//		ServletContextInitializerBeans
+		
+		//DispatcherServlet
+		//DispatcherServletAutoConfiguration
+		
+		
+	}
+}
